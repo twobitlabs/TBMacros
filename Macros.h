5d8be5c72e08d45924ebd3a4a2047444282274a1
Christopher Pickslay <tower@factotum.me>
2011-06-22 12:38:33 -0700

    initial checkin

---
+++ Macros.h
@@ -0,0 +1,68 @@
+static inline BOOL IsEmpty(id thing) {
+    return thing == nil
+        || ([thing respondsToSelector:@selector(length)]
+        && [(NSData *)thing length] == 0)
+        || ([thing respondsToSelector:@selector(count)]
+        && [(NSArray *)thing count] == 0);
+}
+
+static inline NSString *StringFromObject(id object) {
+	if (object == nil || [object isEqual:[NSNull null]]) {
+		return @"";
+	} else if ([object isKindOfClass:[NSString class]]) {
+		return object;
+	} else if ([object respondsToSelector:@selector(stringValue)]){
+		return [object stringValue];
+	} else {
+		return [object description];
+	}
+}
+
+// example usage: UIColorFromHex(0x9daa76)
+#define UIColorFromHex(hexValue) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 green:((float)((hexValue & 0xFF00) >> 8))/255.0 blue:((float)(hexValue & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromRGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+#define UIColorFromRGB(r,g,b) UIColorFromRGBA(r,g,b,1.0)
+
+#define LOG(fmt, ...) NSLog(@"%s:%d (%s): " fmt, __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
+#define IDARRAY(...) (id []){ __VA_ARGS__ }
+#define IDCOUNT(...) (sizeof(IDARRAY(__VA_ARGS__)) / sizeof(id))
+
+#define ARRAY(...) [NSArray arrayWithObjects: IDARRAY(__VA_ARGS__) count: IDCOUNT(__VA_ARGS__)]
+
+#define DICT(...) DictionaryWithIDArray(IDARRAY(__VA_ARGS__), IDCOUNT(__VA_ARGS__) / 2)
+
+//The helper function unpacks the object array and then calls through to NSDictionary to create the dictionary:
+static inline NSDictionary *DictionaryWithIDArray(id *array, NSUInteger count) {
+    id keys[count];
+    id objs[count];
+    
+    for(NSUInteger i = 0; i < count; i++) {
+        keys[i] = array[i * 2];
+        objs[i] = array[i * 2 + 1];
+    }
+    
+    return [NSDictionary dictionaryWithObjects: objs forKeys: keys count: count];
+}
+#define POINTERIZE(x) ((__typeof__(x) []){ x })
+#define NSVALUE(x) [NSValue valueWithBytes: POINTERIZE(x) objCType: @encode(__typeof__(x))]
+
+#ifdef DEBUG
+    #define INFO(fmt, ...) LOG(fmt, ## __VA_ARGS__)
+#else
+    // do nothing
+    #define INFO(fmt, ...) 
+#endif
+
+#define ERROR(fmt, ...) LOG(fmt, ## __VA_ARGS__)
+
+#define NUM_INT(int) [NSNumber numberWithInt:int]
+
+#define CENTER_VERTICALLY(parent,child) (parent.frame.size.height - child.frame.size.height) / 2
+#define WIDTH(view) view.frame.size.width
+#define HEIGHT(view) view.frame.size.height
+#define X(view) view.frame.origin.x
+#define Y(view) view.frame.origin.y
+#define BOTTOM(view) view.frame.origin.y + view.frame.size.height 
+
+#define INDEX_PATH(a,b) [NSIndexPath indexPathWithIndexes:(NSUInteger[]){a,b} length:2]
